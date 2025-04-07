8b54e25affb861a57ae730d92e8f20f0ab7e0c99
joelans <baileyderek59@gmail.com>
2025-04-07 20:46:15 +0300

    feat(header): add header of main page for desktop and mobile screens

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,12 @@
 import './index.css';
 
-import { StrictMode } from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
 
 import App from '~/app/App.tsx';
-import { store } from '~/store/configure-store.ts';
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </StrictMode>,
+    <ChakraProvider>
+        <App />
+    </ChakraProvider>,
 );
